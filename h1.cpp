ed7942bf0a2f8908dfaf3c0b32ec256aca1fa889
Kenishavora <131760404+Kenishavora@users.noreply.github.com>
2023-04-25 15:11:19 +0530

    add

---
+++ h1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"enter the value of a: ";
+    cin>>a;
+    cout<<"\nenter the value of b: ";
+    cin>>b;
+    cout<<a+b<<"\n";
+    cout<<a*b<<"\n";
+    cout<< "the diff ofa-b"<<"\t"<<a-b<<"\n";
+    cout<<a%b<<"\n";
+    cout<<a/b<<"\n";
+    return 0;
+}
